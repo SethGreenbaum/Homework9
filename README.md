1a5fea64dcbc1d70a3362f28499ce24ea32328ee
Seth Greenbaum <sethmgreenbaum@gmail.com>
2020-03-07 15:02:52 -0700

    adding all functionality

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# Homework9
-Repository for Homework 9
+
+# Homework9 
+
+## Created By:
+
+![Profile Image](https://avatars3.githubusercontent.com/u/57598605?v=4)
+
+Seth Greenbaum
+
+*GitHub Username: SethGreenbaum
+
+*Email: sethmgreenbaum@gmail.com
+
+*Location: Denver, CO
+
+*Bio: Process Engineer based in Colorado, specializing in food processing, food science, organic produce, molecular biology, plant biology,  e-commerce, data-analysis
+
+## Description 
+
+A simple note taking app 
+
+## Table of Contents 
+
+* [Installation](#installation) 
+
+* [Usage](#usage) 
+
+* [Contributors](#contributors) 
+
+* [License](#license) 
+
+* [Tests](#tests) 
+
+* [Questions](#questions) 
+
+## Installation 
+
+none  
+
+## Usage 
+
+a simple app for taking and saving notes 
+
+## Credits
+
+DU coding bootcamp
+
+## Contributing 
+
+none 
+
+## License 
+
+none 
+
+## Tests 
+
+console log tests 
+
+## Questions 
+
+none 
+
+
+
+![badge](https://img.shields.io/badge/Generated%20By-README%20Generator-blue)
+
+
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-var express = require("express");
-var path = require("path");
-var app = express();
-var PORT = process.env.PORT || 3000;
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+const app = express();
+const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
 var notes = [];
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
   console.log("homepage")
@@ -15,41 +21,57 @@ app.get("/notes",(req, res) => {
   console.log("notes")
 });
 app.get("/api/notes", (req, res) => {
-  return res.json(notes);
+  readFileAsync(path.resolve(__dirname, "./db/db.json"),"utf-8").then(function(data){
+  res.json(JSON.parse(data));
+});
 });
 app.get("/api/notes/:id", (req, res) => {
   var chosen = req.params.id;
-  console.log(chosen);
-  for (var i = 0; i < notes.length; i++) {
-    if (chosen === notes[i].id) {
-      return res.json(notes[i]);
-    }
-  }
-  return res.json(`${chosen} : note not found`);
+  console.log(chosen+ ": was chosen");
+  readFileAsync(path.resolve(__dirname, "./db/db.json"),"utf-8").then(function(data){
+    var notes = JSON.parse(data); 
+    for (var i = 0; i < notes.length; i++) {
+      if (chosen === notes[i].id) {
+        return res.json(notes[i]);
+      }
+    } return(res.json(`${chosen}: note not found`))
+  });
 });
 app.post("/api/notes", function(req, res) {
     var newNote = req.body;
   
     console.log(newNote.title + "this was added from server side");
-    
+
     newNote.id = newNote.title.replace(/\s+/g, "").toLowerCase();
-    notes.push(newNote);
-  
+
+    readFileAsync(path.resolve(__dirname, "./db/db.json"),"utf-8").then(function(data){
+      var notes = JSON.parse(data); 
+      notes.push(newNote);
+      var ret = JSON.stringify(notes)
+      writeFileAsync(path.resolve(__dirname, "./db/db.json"),ret).then(function(){
+        console.log("successfully logged")
+      })
+    });
     res.json(newNote);
 });
 app.delete("/api/notes/:id", function(req, res) {
     var chosen = req.params.id;
   
-    
-    for (var i = 0; i < notes.length; i++) {
+    readFileAsync(path.resolve(__dirname, "./db/db.json"),"utf-8").then(function(data){
+      var notes = JSON.parse(data); 
+      for (var i = 0; i < notes.length; i++) {
         if (chosen === notes[i].id) {
           notes.splice(i,1);
           console.log(chosen + ":  this was deleted from server side");
         }
       }
-    
+      var ret = JSON.stringify(notes)
+      writeFileAsync(path.resolve(__dirname, "./db/db.json"),ret).then(function(){
+        console.log("successfully logged")
+        res.json(notes)
+      })
+    });
 });
-  
 app.listen(PORT, () => {
     console.log("App listening on http://localhost:" + PORT);
 });
